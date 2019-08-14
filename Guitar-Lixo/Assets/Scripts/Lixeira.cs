b9dad70114601b7b4df83dc6ea95ff44eb714297
FranciscoSabato <francisco_francisco123@outlook.com>
2019-08-14 20:17:39 -0300

    atualizações feitas, animação corrigida, e executaveis para PC e WEB feitos

--- Guitar-Lixo/Assets/Scripts/Lixeira.cs
+++ Guitar-Lixo/Assets/Scripts/Lixeira.cs
@@ -6,10 +6,14 @@ public class Lixeira : MonoBehaviour
 {
     Animator animacao;
     public KeyCode key;
+    float posY;
+    float posX;
 
     void Start()
     {
         animacao = GetComponent<Animator>();
+        posX = transform.position.x;
+        posY = transform.position.y;
     }
     
     void Update()
@@ -21,6 +25,15 @@ public class Lixeira : MonoBehaviour
         else
         {
             animacao.SetBool("Braco", false);
+        }
+
+        if (animacao.GetBool("Braco"))
+        {
+            transform.position = new Vector2(posX, posY * 0.03f);
+        }
+        else
+        {
+            transform.position = new Vector2(posX, posY);
         }
     }
 }
